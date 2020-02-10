db8785b271c3069109c6f4966f041f77e4efd68b
jamievullo <jvullo@protonmail.com>
2020-02-10 09:17:23 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,21 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {
+    friend: []}, action){
+        switch(action.type) {
+            case 'ADD_FRIEND': 
+                return ({...state,
+                friends: [
+                    ...state.friends,
+                    action.friend
+                ]})
+            case 'REMOVE_FRIEND':
+                //wow, ok I have to go thru this a few more times to wrap my head around this
+                const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+                    return ({...state,
+                    friends: [
+                        ...state.friends.slice(0, removalIndex),
+                        ...state.friends.slice(removalIndex + 1)
+            ]})
+            default:
+                return state;
+        }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,14 @@
-export function managePresents(state, action){
+export function managePresents(state = {
+    numberOfPresents: 0,
+}, action){
+
+    switch(action.type) {
+        case 'INCREASE':
+            //had to lookk at Object.assign, only had the nOP and state.nOP + 1, lookig for answers where Object.assign came from
+            return Object.assign({}, state, {
+                numberOfPresents: state.numberOfPresents + 1
+            });
+        default:
+            return state;
+    }
 }
