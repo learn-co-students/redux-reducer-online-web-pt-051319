75de6081bd92f7344e9798ae072b8e85b6ad2255
jasmine1226 <jasmine1226@gmail.com>
2020-02-27 23:21:55 -0800

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      let newFriends = ;
+      return { friends: [
+        ...state.friends,
+action.friend
+]};
+    case "REMOVE_FRIEND":
+      let newFriends = state.friends.splice(action.id, 1);
+      return { friends: newFriends};
+    default:
+      return state;
+  }
 }
